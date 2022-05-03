f5ff46e3ce9303fb4157e7dde53cf9060b524480
James <2009bennettj@gmail.com>
2022-05-03 16:00:36 +0100

    Log expanding animation

--- lib/presentation/pages/log_session/components/log_exercise_widget.dart
+++ lib/presentation/pages/log_session/components/log_exercise_widget.dart
@@ -22,15 +22,22 @@ class LogExerciseWidget extends StatefulWidget {
   State<LogExerciseWidget> createState() => _LogExerciseWidgetState();
 }
 
-class _LogExerciseWidgetState extends State<LogExerciseWidget> {
+class _LogExerciseWidgetState extends State<LogExerciseWidget>
+    with SingleTickerProviderStateMixin {
   final Map<int, int> _completedRepMap = {};
 
+  bool _isExpanded = false;
+
   late int reps;
+
+  late AnimationController animController;
 
   @override
   void initState() {
     super.initState();
     reps = widget._exercise.setsReps.reps;
+    animController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 400));
 
     for (int s = 1; s <= widget._exercise.setsReps.sets; s++) {
       _completedRepMap[s] = reps;
@@ -48,37 +55,57 @@ class _LogExerciseWidgetState extends State<LogExerciseWidget> {
 
     return Container(
       padding: const EdgeInsets.only(bottom: 16),
-      child: Card(
-        elevation: 4,
-        clipBehavior: Clip.antiAlias,
-        child: Container(
-          decoration: BoxDecoration(
-              color: _pplColor.shade50, borderRadius: BorderRadius.circular(4)),
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Column(
-            children: [
-              ExerciseCardHeader(exercise: widget._exercise, pplColor: _pplColor),
-              Divider(color: _pplColor, thickness: 1),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Column(
-                  children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text(Strings.logSetNumber,
-                                style: AppTextStyles.body14),
-                            Text(Strings.logCompletedReps,
-                                style: AppTextStyles.body14)
-                          ],
-                        ),
-                      ] +
-                      _completedSetRows,
-                ),
-              )
-            ],
+      child: GestureDetector(
+        onTap: () {
+          setState(() => _isExpanded = !_isExpanded);
+        },
+        child: Card(
+          elevation: 4,
+          clipBehavior: Clip.antiAlias,
+          child: Container(
+            decoration: BoxDecoration(
+                color: _pplColor.shade50,
+                borderRadius: BorderRadius.circular(4)),
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: Column(children: <Widget>[
+              ExerciseCardHeader(
+                  exercise: widget._exercise, pplColor: _pplColor),
+              AnimatedSize(
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.fastOutSlowIn,
+                  child: buildLogContent(
+                      _isExpanded, _pplColor, _completedSetRows))
+            ]),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildLogContent(bool isExpanded, MaterialColor _pplColor,
+      List<Widget> _completedSetRows) {
+    return SizedBox(
+      height: _isExpanded ? null : 0,
+      child: Column(
+        children: [
+          Divider(color: _pplColor, thickness: 1),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(Strings.logSetNumber, style: AppTextStyles.body14),
+                        Text(Strings.logCompletedReps,
+                            style: AppTextStyles.body14)
+                      ],
+                    ),
+                  ] +
+                  _completedSetRows,
+            ),
+          )
+        ],
       ),
     );
   }
