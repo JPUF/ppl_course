3fe73cc477a50f99994fa6e24d0165404d54b3cd
James <2009bennettj@gmail.com>
2022-05-04 13:50:42 +0100

    Minor homepage refactor

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -19,9 +19,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final BlocBuilder<SessionsBloc, SessionsState> sessionBuilder;
+
   @override
-  Widget build(BuildContext context) {
-    final sessionBuilder = BlocBuilder<SessionsBloc, SessionsState>(
+  void initState() {
+    super.initState();
+    sessionBuilder = BlocBuilder<SessionsBloc, SessionsState>(
         buildWhen: (p, c) => c is AllSessionsState,
         builder: (context, state) {
           if (state is AllSessionsState) {
@@ -43,7 +46,10 @@ class _HomePageState extends State<HomePage> {
             return const SizedBox(height: 16);
           }
         });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -52,50 +58,54 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Stack(
           alignment: AlignmentDirectional.bottomEnd,
-          children: [
-            SizedBox(
-              height: double.infinity,
-              width: double.infinity,
-              child: SingleChildScrollView(
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        const SizedBox(height: 8),
-                        sessionBuilder,
-                        const SizedBox(height: 16),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+          children: [buildHomePageContent(), buildPlanSessionButton(context)],
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildHomePageContent() {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 8),
+                sessionBuilder,
+                const SizedBox(height: 16),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-              child: AccentButton(
-                text: Strings.planSessionCTA,
-                onTap: () => Navigator.of(context).pushNamed(
-                    Destination.planSession,
-                    arguments: SessionArgs(null)),
-                endIcon: SvgPicture.asset(
-                  'assets/images/ic_dumbbell.svg',
-                  width: 24,
-                  height: 24,
-                ),
-              ),
-            )
-          ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding buildPlanSessionButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+      child: AccentButton(
+        text: Strings.planSessionCTA,
+        onTap: () => Navigator.of(context)
+            .pushNamed(Destination.planSession, arguments: SessionArgs(null)),
+        endIcon: SvgPicture.asset(
+          'assets/images/ic_dumbbell.svg',
+          width: 24,
+          height: 24,
         ),
       ),
     );
   }
 
   onTapLogSession(Session sessionToLog) {
-    Navigator.of(context).pushNamed(Destination.logSession,
-        arguments: sessionToLog);
+    Navigator.of(context)
+        .pushNamed(Destination.logSession, arguments: sessionToLog);
   }
 
   onTapEditSession(Session sessionToEdit) {
