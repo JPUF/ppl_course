04c9951a6714a097cc5321d409df670a8efb16fe
James <2009bennettj@gmail.com>
2022-05-11 11:17:48 +0100

    Sample with Google Charts lib

---
+++ lib/data/models/session/date_time_weight.dart
@@ -0,0 +1,8 @@
+import 'package:ppl_course/data/models/exercise/weight.dart';
+
+class DateTimeWeight {
+  final DateTime dateTime;
+  final Weight weight;
+
+  DateTimeWeight(this.dateTime, this.weight);
+}
--- lib/data/models/session/session_log.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ppl_course/common/utils/date_time_day.dart';
-import 'package:ppl_course/data/models/exercise/weight.dart';
-
-class SessionLog {
-  final Weight totalVolume;
-  final DateTimeDay dateTime;
-
-  SessionLog(this.totalVolume, this.dateTime);
-}
